55483173b99f0698c80d7b63e0fbd370c8490170
MudhithaK <mudhithakalpani08@gmail.com>
2024-10-13 12:19:34 +0530

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+class operators{
+
+	public static void main(String[] args){
+		float a,b;
+		Scanner scn= new Scanner(System.in);
+		/*System.out.println("Enter two Numbers..");
+		System.out.print("Number 1 :");
+		a= scn.nextFloat();
+		System.out.print("Number 2 :");
+		b= scn.nextFloat();
+		System.out.println("This is the out put from operators...");
+
+		System.out.println("Addition of the numbers ="+(a+b));
+		System.out.println("Reduction of the numbers ="+(a-b));
+		System.out.println("Multiplication of the numbers = "+(a*b));
+		System.out.println("Division of the numbers ="+(a/b));
+		System.out.println("Modulus of the numbers ="+(a%b));		
+
+		System.out.println("Prefix of Number 1 ="+ (++a));
+		System.out.println("Postfix of Number 2="+(a++));*/
+
+
+
+		/*int r=10 + 5*2;
+		float r2=(10/2+3*4);
+		float r3= (10/(2+3)*4);
+		float r4=(10+5-3*2);
+		float r5=(10+(5-3)*2) ;
+		float r6=(10-5+3*2/1);
+		float r7=(10-5+3)*2/1;
+		float r8=(5+2)*(3-1);
+		float r9=10/2+4%3;
+		float r10=10-2+4*3/2%5;
+		float r11=(10-2+4)*(3/2)%5;
+		System.out.println("result= "+r);
+		System.out.println("10/2 + 3 * 4 :"+r2 );
+		System.out.println("10/(2+3)*4 :"+ r3);
+		System.out.println("10+5-3*2 :"+r4);
+		System.out.println("10+(5-3)*2) :"+r5);
+		System.out.println("10-5+3*2/1 :"+r6);
+		System.out.println("(10-5+3)*2/1 :"+r7);
+		System.out.println("(5+2)*(3-1) :"+r8);
+		System.out.println("10/2+4%3 :"+r9);
+		System.out.println("10-2+4*3/2%5 :"+r10);
+		System.out.println("(10-2+4)*(3/2)%5 :"+r11);
+		
+
+
+		int y=5;
+		int R2= 5+3 * x++;
+		int R3= 10+3*y++ - --y;
+		System.out.println("result ="+R3);*/
+			
+		int n,m;
+		System.out.println("Enter two Numbers..");
+		System.out.print("Number 1 :");
+		n= scn.nextInt();
+		System.out.print("Number 2 :");
+		m= scn.nextInt();
+		boolean result= n==m;
+		System.out.println(n+"=="+m+":"+result);
+		
+
+	}
+
+
+
+
+
+}
