dc0d3ea2305d472c27419cd00bda306a3293c909
MudhithaK <mudhithakalpani08@gmail.com>
2024-12-11 17:58:22 +0000

    react

---
+++ frontend/src/navigate/index.jsx
@@ -0,0 +1,12 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {header} from "../component/header";
+
+const navigate=() => {
+    return{
+       
+
+
+    }
+}
+
+export default navigate;
