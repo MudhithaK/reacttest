b284e005d0d674f1697f49f1fcf79801fbf7c55f
MudhithaK <mudhithakalpani08@gmail.com>
2024-12-12 05:39:08 +0000

    react

--- frontend/src/component/header.jsx
+++ frontend/src/component/header.jsx
@@ -1,5 +1,24 @@
 export const header =() =>{
- return{
-    
- }
-}
+ return(
+    <nav id="header" className="bg-black text-white">
+    <div className="w-full container max-auto flex flex-wrap items-center justify-content-between mt-0 py-2">
+
+        <div classNmae="logo-wrapper pl-4 flex items-center">
+            <img src={"/"} alt="logo"/>
+        </div>
+
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+
+    </div>
+    </nav>
+ );
+
+};
