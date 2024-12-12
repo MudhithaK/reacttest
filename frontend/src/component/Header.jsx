9e992d73c5a06380f7f9cbffbae4edcdc8fb2c31
MudhithaK <mudhithakalpani08@gmail.com>
2024-12-12 15:48:00 +0000

    reat

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,10 +1,14 @@
+import {Link} from "react-router-dom";
 export const Header =() =>{
  return(
     <nav id="header" className="bg-black text-white">
     <div className="w-full container max-auto flex flex-wrap items-center justify-content-between mt-0 py-2">
 
         <div classNmae="logo-wrapper pl-4 flex items-center">
-            <img src={"/"} alt="logo"/>
+           <Link to="/">
+           <img src={"/"} alt="logo"/>
+           </Link>
+           
         </div>
 
         <div className="nev-menu-wrapper flex item-center justify-between space-x-10">
