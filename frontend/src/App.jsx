5a7a7473a8968c52dce37a4f2ecbfe125b008d98
MudhithaK <mudhithakalpani08@gmail.com>
2024-12-12 15:35:40 +0000

    reat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import './App.css';
 import { Header } from './component/Header';
-
+import Navigate from './navigate';
 
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Navigate/>
     </div>
  
   );
