9d34b3176db67edf9c82d90becf08bc22e33fca6
MudhithaK <mudhithakalpani08@gmail.com>
2024-12-16 13:42:37 +0000

    react

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,6 +1,19 @@
 const Login = () =>{
     return(
-        <>Login...</>
+        <>Login...
+        
+        <di>
+            <form>
+                <lable>Name :</lable>
+                <input type="text" placeholder="enter your name"></input>
+                <br></br>
+                <lable>Email :</lable>
+                <input type="text" placeholder="Email Address.."></input>
+            </form>
+        </di>
+        
+        </>
+
 
     );
 }
